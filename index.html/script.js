508377baf2be72eedb24c98cd8d05907bcf562d7
lapala72 <deleonromi0@gmail.com>
2024-05-19 00:10:08 -0300

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,10 @@
+// Este es un comentario en JavaScript
+
+// Definición de una función que suma dos números
+function suma(a, b) {
+    return a + b;
+}
+
+// Llamada a la función suma
+var resultado = suma(5, 3);
+console.log("El resultado de la suma es: " + resultado);
